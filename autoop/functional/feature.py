a2c153c99f02255bde813d6bb2227e67695a43a3
kiki57585 <k.wattez@student.rug.nl>
2024-11-10 15:24:23 +0100

    more flake8

--- autoop/functional/feature.py
+++ autoop/functional/feature.py
@@ -1,6 +1,8 @@
 from typing import List
 from autoop.core.ml.dataset import Dataset
 from autoop.core.ml.feature import Feature
+import pandas as pd
+import io
 
 
 def detect_feature_types(dataset: Dataset) -> List[Feature]:
@@ -15,7 +17,7 @@ def detect_feature_types(dataset: Dataset) -> List[Feature]:
     data = dataset.read()
 
     data = pd.read_csv(io.StringIO(data.decode("utf-8")))
-    
+
     for column in data.columns:
         if all(isinstance(value, (float, int))
                for value in data[column]):
